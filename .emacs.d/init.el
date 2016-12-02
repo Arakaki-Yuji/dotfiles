9d3ad81c54697a7b2b5390d99f7986199f44fbd4
vagrant <vagrant@localhost.localdomain>
2016-12-02 09:49:34 +0000

    init commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,24 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+
+(load-theme 'tango-dark t)
+
+(global-set-key "\C-h" 'delete-backward-char)
+(global-whitespace-mode t)
+(line-number-mode t)
+(setq frame-title-format "%f")
+(global-linum-mode 0)
+
+(add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+
+(require 'helm)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
